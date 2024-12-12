01ff4536b08615f85b93192ad14f2b0034753d33
swyoo5 <3040sw@naver.com>
2024-12-12 16:23:47 +0900

    piechart

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Pie } from "react-chartjs-2";
 import chart from "../styles/chart";
+
+
+
 function PieChart({data}) {
+
+
+
     return (
         <div style={chart.Pie}>
             <h2>Assets Distribution</h2>
